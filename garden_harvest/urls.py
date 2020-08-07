c01c15fc5da4f6590342b6a62fc3560377a09e04
Julian West <west221b@gmail.com>
2020-08-07 10:07:07 -0400

    Put authentication on api url path

--- garden_harvest/urls.py
+++ garden_harvest/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('users.urls')),
+    path('api/', include('users.urls')),
 ]
