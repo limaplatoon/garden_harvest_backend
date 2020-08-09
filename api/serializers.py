9deedbf7ebf14db66e5f0ab3d652923039939c74
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-08-08 22:32:11 -0500

    created the calendar url, view, and serializer. added queryset to UserPlants view as it was throwing errors that it was missing

--- api/serializers.py
+++ api/serializers.py
@@ -81,3 +81,42 @@ class PlantSlotSerializer(serializers.ModelSerializer):
             'harvest_date_max',
         ]
 
+
+class CalendarPlantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = ['id','common_name','harvest_max','harvest_min','sowing','spacing']
+
+
+class CalendarSlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Slot
+        fields = ['id', 'color','location_description','name']
+
+
+class CalendarZoneSerializer(serializers.ModelSerializer):
+    plant = CalendarPlantSerializer()
+
+    class Meta:
+        model = PlantZone
+        fields = ['calendar', 'plant']
+
+
+class CalendarSerializer(serializers.ModelSerializer):
+    plant_zone = CalendarZoneSerializer()
+    slot = CalendarSlotSerializer()
+    planned_duration = serializers.SerializerMethodField()
+    requires_seeding = serializers.SerializerMethodField()
+
+    def get_planned_duration(self, instance):
+        harvest_max = instance.plant_zone.plant.harvest_max
+        calendar = instance.plant_zone.calendar
+        return (harvest_max + (56 if 'S' in calendar else 14))
+
+    def get_requires_seeding(self, instance):
+        return ('S' in instance.plant_zone.calendar)
+
+    class Meta:
+        model = PlantSlot
+        fields = ['id','created_at', 'date_planted', 'date_seeded', 'harvest_date_max', 'harvest_date_min', 'planned_duration', 'plant_zone', 'requires_seeding', 'slot']
+
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import ListAvailablePlants, PlantDetail, UserPlants
+from .views import ListAvailablePlants, PlantDetail, Calendar, UserPlants
 
 urlpatterns = [
     path('suggested/', ListAvailablePlants.as_view()),
     path('plants/<int:pk>/', PlantDetail.as_view()),
-    path('myplants/', UserPlants.as_view())
+    path('myplants/', UserPlants.as_view()),
+    path('calendarEvents/', Calendar.as_view())
 ]
